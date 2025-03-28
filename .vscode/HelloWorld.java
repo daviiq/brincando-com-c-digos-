80d7b452054469a993999911c77c2c08f79c17d1
Davi <160233282+daviiq@users.noreply.github.com>
2025-03-28 14:13:30 +0000

    Hello World !

---
+++ .vscode/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+
+  public static void main(String[] args) {
+    System.out.println("Hello, World!");
+    System.out.println("Hello, World!");
+  }
+}
+
+public class main {
+  public static
+}
