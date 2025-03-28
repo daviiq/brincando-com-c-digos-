8591dc1f240b7a6c44bfcbc60db50ca0d2dfb3a0
Davi <160233282+daviiq@users.noreply.github.com>
2025-03-28 17:03:08 +0000

    Calculadora Pronto

--- .vscode/main.java
+++ .vscode/main.java
@@ -1,20 +1,49 @@
-//Calculcadora
-
 import java.util.Scanner;
 
 public class main {
-
   public static void main(String[] args) {
-    System.out.print("Digite o primeiro número: ");
-    double num1 = Scanner.nextDouble();
-
-    System.out.print("Digite a operação (+, -, *, /): ");
-    char operacao = Scanner.next().charAt(0);
-
-    System.out.print("Digite o segundo número: ");
-    double num2 = Scanner.nextDouble();
+    Scanner scanner = new Scanner(System.in);
     
-    double resultado = 0;
+    // digitar o número 1:
 
-}
+    System.out.print("Digite n1: ");
+    double num1 = scanner.nextDouble();
+    
+    //Escolher a operação matemática:
+
+    System.out.print("eslconhe a operação (+, -, *, /): ");
+    char operacao = scanner.next().charAt(0);
+    
+    //Digitar o número 2:
+    System.out.print("Digite n2: ");
+    double num2 = scanner.nextDouble();
+
+    //Resultado:
+    double resultado;
+    switch (operacao) {
+      case '+':
+        resultado = num1 + num2;
+        break;
+      case '-':
+        resultado = num1 - num2;
+        break;
+      case '*':
+        resultado = num1 * num2;
+        break;
+      case '/':
+        if (num2 != 0) {
+          resultado = num1 / num2;
+        } else {
+          System.out.println("Erro: Nao sabes cacular em java!");
+          return;
+        }
+        break;
+      default:
+        System.out.println("vai se foder deu exeçao nao prevista no incontrol!");
+        return;
+    }
+    // Exibir o resultado:
+    System.out.println("Resultado: " + resultado);
+    scanner.close();
+  }
 }
