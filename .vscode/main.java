7a2d3a07ebde63d652a29160bafb9f97f68aca88
Davi <160233282+daviiq@users.noreply.github.com>
2025-03-28 14:57:47 +0000

    Calculadora

---
+++ .vscode/main.java
@@ -0,0 +1,20 @@
+//Calculcadora
+
+import java.util.Scanner;
+
+public class main {
+
+  public static void main(String[] args) {
+    System.out.print("Digite o primeiro número: ");
+    double num1 = Scanner.nextDouble();
+
+    System.out.print("Digite a operação (+, -, *, /): ");
+    char operacao = Scanner.next().charAt(0);
+
+    System.out.print("Digite o segundo número: ");
+    double num2 = Scanner.nextDouble();
+    
+    double resultado = 0;
+
+}
+}
